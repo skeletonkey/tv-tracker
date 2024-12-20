20629345b4affaa7b0bdd3f9af740074c66b5c0c
Erik Tank <shiny.gift6738@fastmail.com>
2024-12-19 20:13:40 -0700

    getting the front end working - search

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,26 +1,31 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+<script setup>
+import { ref, computed } from 'vue'
+import Home from './pages/HomePage.vue'
+import About from './pages/AboutPage.vue'
+import NotFound from './pages/NotFoundPage.vue'
+import Search from './pages/SearchPage.vue'
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+const routes = {
+  '/': Home,
+  '/about': About,
+  '/search': Search
 }
+
+const currentPath = ref(window.location.hash)
+
+window.addEventListener('hashchange', () => {
+  currentPath.value = window.location.hash
+})
+
+const currentView = computed(() => {
+  return routes[currentPath.value.slice(1) || '/'] || NotFound
+})
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+  <a href="#/">Home</a> |
+  <a href="#/search">Search</a> |
+  <a href="#/about">About</a> |
+  <a href="#/non-existent-path">Broken Link</a>
+  <component :is="currentView" />
+</template>
