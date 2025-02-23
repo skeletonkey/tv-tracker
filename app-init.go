b12b89c531b521639f760f8d69a1b2468084e4a8
Erik Tank <shiny.gift6738@fastmail.com>
2025-02-23 07:05:15 -0700

    add server package

--- app-init.go
+++ app-init.go
@@ -11,6 +11,6 @@ func main() {
 		app.WithDependencies("github.com/pioz/tvdb"),
 		app.WithGoVersion("1.23"),
 		app.WithMakefile(),
-		app.WithPackages("db", "tvdb"),
+		app.WithPackages("db", "server", "tvdb"),
 	).Generate()
 }
