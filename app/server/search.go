d01164896900936b40292190e625de49cd25f67e
Erik Tank <shiny.gift6738@fastmail.com>
2025-03-23 19:13:05 -0700

    update search

--- app/server/search.go
+++ app/server/search.go
@@ -2,15 +2,22 @@ package server
 
 import (
 	"net/http"
+	"strconv"
 
 	echo "github.com/labstack/echo/v4"
 
+	"github.com/skeletonkey/lib-core-go/logger"
 	"github.com/skeletonkey/tv-tracker/app/tvdb"
 )
 
 func searchHandler(c echo.Context) error {
+	log := logger.Get()
+	log.Trace().Msg("searchHandler")
+
 	query := c.Param("query")
-	res, err := tvdb.Search(query)
+	no_cache, _ := strconv.ParseBool(c.QueryParam("no_cache"))
+
+	res, err := tvdb.Search(query, no_cache)
 	if err != nil {
 		return echo.NewHTTPError(http.StatusBadRequest, err)
 	}
