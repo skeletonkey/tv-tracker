85a015bf7b425b56818ccc780160b85dd1fb85ba
Erik Tank <shiny.gift6738@fastmail.com>
2025-03-23 20:36:27 -0700

    update structs to new db

--- app/server/struct.go
+++ app/server/struct.go
@@ -11,6 +11,7 @@ type User struct {
 	UUID         string `json:"uuid"`
 	Username     string `json:"username" validate:"required"`
 	Email        string `json:"email" validate:"omitempty,email"`
+	Active       bool   `json:"active"`
 	Password     string `json:"password" validate:"required,min=8,max=20"`
 	PasswordConf string `json:"password_confirm" validate:"omitempty,eqfield=Password"`
 }
@@ -21,8 +22,6 @@ type Show struct {
 	Name        string `json:"name"`
 	Type        string `json:"type"`
 	Description string `json:"description"`
-	ExternalID  int    `json:"external_id"`
-	Thumbnail   string `json:"thumbnail"`
 }
 
 type Episode struct {
@@ -33,18 +32,77 @@ type Episode struct {
 	Season        int    `json:"season"`
 	EpisodeNumber int    `json:"episode_number"`
 	AirDate       string `json:"air_date"`
-	ExternalID    int    `json:"external_id"`
 }
 
-type ExternalSource struct {
+type SourceXInternal struct {
+	ID         int    `json:"id"`
+	ShowID     int    `json:"show_id"`
+	EpisodeID  int    `json:"episode_id"`
+	SourceID   int    `json:"source_id"`
+	ExternalID string `json:"external_id"`
+}
+
+type Art struct {
+	ID        int    `json:"id"`
+	URL       string `json:"url"`
+	ArtTypeID int    `json:"art_type_id"`
+	Height    int    `json:"height"`
+	Width     int    `json:"width"`
+}
+
+type ArtType struct {
 	ID   int    `json:"id"`
 	Name string `json:"name"`
 }
 
-type ExternalXInternal struct {
-	ID               int    `json:"id"`
-	ShowID           int    `json:"show_id"`
-	EpisodeID        int    `json:"episode_id"`
-	ExternalSourceID int    `json:"external_source_id"`
-	ExternalID       string `json:"external_id"`
+type EntityArt struct {
+	ID         int  `json:"id"`
+	ShowID     int  `json:"show_id"`
+	EpisodeID  int  `json:"episode_id"`
+	ArtID      int  `json:"art_id"`
+	DefaultArt bool `json:"default_art"`
+}
+
+type Source struct {
+	ID     int    `json:"id"`
+	Name   string `json:"name"`
+	APIURL string `json:"api_url"`
+	Site   string `json:"site"`
+}
+
+type Status struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+	Type string `json:"type"`
+}
+
+type UserXEntityArt struct {
+	ID          int `json:"id"`
+	EntityArtID int `json:"entity_art_id"`
+	UserID      int `json:"user_id"`
+}
+
+type UserXShow struct {
+	ID      int  `json:"id"`
+	UserID  int  `json:"user_id"`
+	ShowID  int  `json:"show_id"`
+	Rank    int  `json:"rank"`
+	Visible bool `json:"visible"`
+}
+
+type UserXSource struct {
+	ID       int    `json:"id"`
+	UserID   int    `json:"user_id"`
+	SourceID int    `json:"source_id"`
+	URL      string `json:"url"`
+	APIToken string `json:"api_token"`
+}
+
+type UserHistory struct {
+	ID        int    `json:"id"`
+	UserID    int    `json:"user_id"`
+	ShowID    int    `json:"show_id"`
+	EpisodeID int    `json:"episode_id"`
+	StatusID  int    `json:"status_id"`
+	Timestamp string `json:"timestamp"` // or time.Time if you want to use Go's time package
 }
